40acb94ff770c0a4bec78a1eb88f12be1ffe94ca
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2022-09-19 10:37:32 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log("hi");
+
+// IPO Brightness Calculator By Oliver Lo
+
+// Input
+let R = +prompt("Enter a R color value (Red):");
+let G = +prompt("Enter a G color value (Green):");
+let B = +prompt("Enter a B color value (Blue):");
+
+// Process
+let brightness = Math.sqrt(0.299 * (R) ** 2 + 0.587 * (G) ** 2 + 0.114 * (B) ** 2)
+
+// Output
+alert(`The brightness is ${brightness}.`)
